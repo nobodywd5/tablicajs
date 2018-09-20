7b860ee6c10693f4cd50e4f3d955d5ad04c00d0a
Aleksandr Kostiuk <kosskill2011@gmail.com>
2018-09-20 15:04:11 +0200

    tablica

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-    document.write('<table border="1" style="border-collapse: collapse; text-align: center">');
-for(var i=0; i<10; i++) {
-    document.write('</sup></td><td style="width: 30px">'+i+'</sup></td><td style="width: 30px">'+Math.pow(1,i)+i+'</td></tr>');
-}
-    document.write('</table>');
-
-
-
+var files = ["Music", "Document", "Video" "Foto"]
